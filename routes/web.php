89e21953d433cd61b8686ec43d68147fc2876b2b
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-05-13 13:12:10 +0300

    Controller Oluşturma

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AnasayfaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -9,32 +10,8 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the "web" middleware group. Now create someth
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('merhaba',function(){
-    return "Merhaba";
-});
-
-Route::get('api/v1/merhaba',function(){
-    return ['mesaj'=>'Merhaba'];
-});
-Route::get('/urun/{isim}/{id?}',function($urunadi,$id=0){
-   return "Ürün Adı: $id $urunadi";
-})->name('urun_detay');
-
-Route::get('/kampanya',function (){
-   return redirect()->route('urun_detay', ['isim'=>'elma', 'id'=>5]);
-});
-Route::get('/kampanya1/{name}/{yas}',function ($name,$yas){
-   return "feride34: ".$name. " yas:".$yas;
-})->name('feride');
-
-Route::get('/kampanya2',function (){
-    return redirect()->route('feride',['name'=>'Nurullah', 'yas'=>26]);
-});
-
+Route::get('/', [AnasayfaController::class,'index'] )->name('anasayfa');
