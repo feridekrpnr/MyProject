619d5445614fc2c64d5c9ccf0f147ea78a9d363c
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-05-13 12:00:48 +0300

    Route Tanımlamaları

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('merhaba',function(){
+    return "Merhaba";
+});
+
+Route::get('api/v1/merhaba',function(){
+    return ['mesaj'=>'Merhaba'];
+});
+Route::get('/urun/{isim}/{id?}',function($urunadi,$id=0){
+   return "Ürün Adı: $id $urunadi";
+})->name('urun_detay');
+
+Route::get('/kampanya',function (){
+   return redirect()->route('urun_detay', ['isim'=>'elma', 'id'=>5]);
+});
+Route::get('/kampanya1/{name}/{yas}',function ($name,$yas){
+   return "feride34: ".$name. " yas:".$yas;
+})->name('feride');
+
+Route::get('/kampanya2',function (){
+    return redirect()->route('feride',['name'=>'Nurullah', 'yas'=>26]);
+});
+
