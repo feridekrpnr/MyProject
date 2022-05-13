4d0659dc42dec64eca2aa4bd262fdc0b9366af67
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2022-05-13 13:22:16 +0300

    Controller Oluşturuldu

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,9 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create someth
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', [AnasayfaController::class,'index'] )->name('anasayfa');
+
